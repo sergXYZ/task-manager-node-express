c1acdfc0964eb8527718b8ce818d6376ef395432
sergXYZ <serhiis1993@gmail.com>
2026-03-23 11:53:50 +0100

    add the rest CRUD functionality

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,10 +1,25 @@
+const { response } = require('express')
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const getTask = (req, res) => {
-  res.send('get task')
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskID}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 const createTask = async (req, res) => {
   try {
@@ -14,11 +29,34 @@ const createTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      returnDocument: 'after',
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask }
--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const connectDB = (uri) => {
   mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useFindAndModify: false,
+    //useCreateIndex: true,
   })
 }
 module.exports = connectDB
